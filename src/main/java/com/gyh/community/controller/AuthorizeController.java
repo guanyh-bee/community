3a9836828116c4f4faa4fee3749c8370caf9822c
gyh <543841473@qq.com>
2020-07-09 09:35:02 +0800

    third

--- src/main/java/com/gyh/community/controller/AuthorizeController.java
+++ src/main/java/com/gyh/community/controller/AuthorizeController.java
@@ -1,5 +1,9 @@
 package com.gyh.community.controller;
 
+import com.gyh.community.dto.AccessTokenDTO;
+import com.gyh.community.dto.GithubUser;
+import com.gyh.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
     @GetMapping("callback")
     public String callback(@RequestParam("code")String code,@RequestParam("state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.17b170297832cc61");
+        accessTokenDTO.setClient_secret("4f63b4e0015557ce11572e9bc4f37cf06ce26b49");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
+
+
 }
