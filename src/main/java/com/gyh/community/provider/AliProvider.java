842ab8e8b479d0332173ee22f37b9c02e2708598
guanyh-bee <543841473@qq.com>
2020-09-06 23:05:10 +0800

    1

---
+++ src/main/java/com/gyh/community/provider/AliProvider.java
@@ -0,0 +1,14 @@
+package com.gyh.community.provider;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AliProvider {
+
+
+
+
+
+}
--- src/test/java/com/gyh/community/CommunityApplicationTests.java
+++ src/test/java/com/gyh/community/CommunityApplicationTests.java
@@ -1,14 +1,50 @@
 package com.gyh.community;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.BucketMetadata;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectResult;
 import com.gyh.community.model.Comment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayInputStream;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
+
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
+        String accessKeyId = "LTAI4G2Vg3St9JPqYR7nHgUV";
+        String accessKeySecret = "qm9qtf9ahcatv17O1Ah5m3XQcVuqnS";
+        String bucketName = "walmm";
+// <yourObjectName>上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
+        String objectName = "second"+ UUID.randomUUID()+".jpg";
+
+// 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+
+
+
+
+
+
+
+// 上传文件到指定的存储空间（bucketName）并将其保存为指定的文件名称（objectName）。
+        String content = "Hello OSS";
+        PutObjectResult putObjectResult = ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content.getBytes()));
+
+
+// 关闭OSSClient。
+        ossClient.shutdown();
+
 
 
     }
