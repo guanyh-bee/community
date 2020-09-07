b11388a03b475157bf517267a190fb30fa522122
gyh <543841473@qq.com>
2020-09-07 10:33:21 +0800

    2

--- src/main/java/com/gyh/community/provider/UploadProvider.java
+++ src/main/java/com/gyh/community/provider/UploadProvider.java
@@ -1,11 +1,10 @@
 package com.gyh.community.provider;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClientBuilder;
+
 import org.springframework.stereotype.Component;
 
 @Component
-public class AliProvider {
+public class UploadProvider {
 
 
 
